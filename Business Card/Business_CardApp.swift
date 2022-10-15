f248fc2d5c435c4377eb9db154991ae7915962a0
Tim Bausch <49855076+baus8650@users.noreply.github.com>
2022-10-15 13:08:25 -0700

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by Tim Bausch on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
