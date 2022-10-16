bf38914ca695184b1f29ca0356b70125ce2034b4
Tim Bausch <49855076+baus8650@users.noreply.github.com>
2022-10-16 00:22:05 -0700

    Remove unnecessary code

--- Business Card/Views/ContentView.swift
+++ Business Card/Views/ContentView.swift
@@ -12,24 +12,28 @@ struct ContentView: View {
     @ObservedObject private var userViewModel = UserViewModel()
     
     var body: some View {
-        VStack {
-            Text(userViewModel.user.name)
-                .font(.title)
-            WebImage(url: URL(string: userViewModel.baseURLString+Networking.NetworkRequests.headShot.rawValue+userViewModel.user.headshot))
-                .resizable()
-                .placeholder {
-                    ProgressView()
-                }
-                .aspectRatio(contentMode: .fit)
-                .frame(maxWidth: 132)
-                .cornerRadius(16)
-            ScrollView {
-                Text(userViewModel.user.biography.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression)
-                    .replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression)
+        NavigationView {
+            VStack {
+//                Text(userViewModel.user.name)
+//                    .font(.title)
+                WebImage(url: URL(string: userViewModel.baseURLString+Networking.NetworkRequests.headShot.rawValue+userViewModel.user.headshot))
+                    .resizable()
+                    .placeholder {
+                        ProgressView()
+                    }
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 132)
+                    .cornerRadius(16)
+                ScrollView {
+                    Text(userViewModel.user.biography.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression)
+                        .replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression)
                     )
+                }
             }
+            .padding()
+            .navigationTitle(userViewModel.user.name)
+            .navigationBarTitleDisplayMode(.large)
         }
-        .padding()
     }
 }
 
