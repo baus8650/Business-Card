e46af8c00a412ac1ad1d3a862a7744c4128c7cf8
Tim Bausch <49855076+baus8650@users.noreply.github.com>
2022-10-15 17:51:44 -0700

    Revise UserViewModel and ContentView to use WebImage

--- Business Card/View Models/UserViewModel.swift
+++ Business Card/View Models/UserViewModel.swift
@@ -4,7 +4,6 @@ import SwiftUI
 
 class UserViewModel: ObservableObject {
     @Published var user = User(name: "", biography: "", isEmployed: false, headshot: "")
-    @Published var headshot: Image = Image(systemName: "person.fill")
     
     var anyCancellables = Set<AnyCancellable>()
     
@@ -16,14 +15,6 @@ class UserViewModel: ObservableObject {
                 
             } receiveValue: { [self] users in
                 self.user = users.first ?? User(name: "", biography: "", isEmployed: false, headshot: "")
-                print(user.biography)
-                let imageURL = URL(string: baseURLString+Networking.NetworkRequests.headShot.rawValue+self.user.headshot)
-                DispatchQueue.global().async {
-                    let imageData = try? Data(contentsOf: imageURL!)
-                    DispatchQueue.main.async {
-                        self.headshot = Image(uiImage: UIImage(data: imageData!)!)
-                    }
-                }
             }
             .store(in: &anyCancellables)
     }
