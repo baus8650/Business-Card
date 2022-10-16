606fc2da045addf761138cd52093dd6784984fd4
Tim Bausch <49855076+baus8650@users.noreply.github.com>
2022-10-15 17:48:22 -0700

    Add Project, Review, ScreenShot, and Skill models

---
+++ Business Card/Models/Project.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftUI
+
+struct Project: Codable, Identifiable {
+    let id: String
+    let name: String
+    let yearCompleted: Int
+    let shortDescription: String
+    let longDescription: String
+    let repoLink: String?
+    let appLink: String?
+    let appIcon: String?
+    let applicationType: ApplicationType
+}
+
+struct ProjectFormatted: Identifiable {
+    let id: String
+    let name: String
+    let yearCompleted: Int
+    let shortDescription: String
+    let longDescription: String
+    let repoLink: String?
+    let appIcon: UIImage
+    let applicationType: ApplicationType
+}
+
+struct Projects: Codable {
+    let projects: [Project]
+}
---
+++ Business Card/Models/ScreenShot.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct ScreenShot: Codable {
+    let id: UUID
+    let description: String
+    let image: String
+}
+
